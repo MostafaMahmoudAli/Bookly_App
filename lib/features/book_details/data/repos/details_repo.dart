da5e41972a8d4b7f196babd4515ca8e7a4a9e29b
mostafa <mostafa.mahmoud1649@gmail.com>
2023-05-25 16:57:22 +0200

    create details repo,details repo impl & done the method of fetching create details cubit & states

---
+++ lib/features/book_details/data/repos/details_repo.dart
@@ -0,0 +1,8 @@
+import 'package:bookly_app/features/layout/data/models/books_model/books_model.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failure.dart';
+
+abstract class DetailsRepo{
+  Future<Either<Failure,List<BooksModel>>>fetchDetailsBooks();
+}
